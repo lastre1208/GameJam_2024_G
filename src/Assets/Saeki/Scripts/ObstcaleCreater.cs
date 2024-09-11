73ba3967123666df1993dca8488d84d0732a12cf
saeki7036 <114328780+saeki7036@users.noreply.github.com>
2024-09-11 15:25:27 +0900

    Fix/using

--- src/Assets/Saeki/Scripts/ObstcaleCreater.cs
+++ src/Assets/Saeki/Scripts/ObstcaleCreater.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class ObstcaleCreater : MonoBehaviour
 {
